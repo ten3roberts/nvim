6f8455c78fe0bf27b19171b64ee7b1eba51238a2
Tei Roberts <ten3roberts@gmail.com>
2023-07-01 14:37:34 +0200

    fix: dial signed

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -192,10 +192,10 @@ return {
       require("dial.config").augends:register_group {
         default = {
           augend.decimal_fraction.new {
-            signed = false,
+            signed = true,
             point_char = ".",
           },
-          augend.integer.alias.decimal, -- nonnegative decimal number (0, 1, 2, 3, ...)
+          augend.integer.alias.decimal_int, -- nonnegative decimal number (0, 1, 2, 3, ...)
           augend.constant.alias.bool,
           augend.integer.alias.hex, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
           augend.semver.alias.semver, -- nonnegative hex number  (0x01, 0x1a1f, etc.)
