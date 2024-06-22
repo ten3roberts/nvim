2f5a66a20766b4a5ab111e0eeb55c3c91bb196e7
Tei Leelo Roberts <ten3roberts@gmail.com>
2024-06-22 19:14:48 +0200

    feat: trouble

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -2,7 +2,7 @@ return {
   {
     "zbirenbaum/copilot.lua",
     lazy = true,
-    event = "InsertEnter",
+    -- event = "InsertEnter",
     config = function()
       require("copilot").setup {
         suggestion = {
--- lua/plugins/flash.lua
+++ lua/plugins/flash.lua
@@ -2,7 +2,7 @@ return {
   "folke/flash.nvim",
   event = "VeryLazy",
   ---@type Flash.Config
-  opts = { modes = { search = { enabled = false } } },
+  opts = {},
   keys = {
     {
       "s",
